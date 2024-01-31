dbe7e94cc8465927bb060e56b59ee75b2ffaa5ce
Amanda Baran <amandabaran9@gmail.com>
2024-01-31 12:33:04 -0500

    scripts update

--- scripts/spin_exp.sh
+++ scripts/spin_exp.sh
@@ -25,10 +25,6 @@ build() {
   tmp=$(pwd)
   cd ../../rome/scripts
   python rexec.py -n ${nodefile} --remote_user=adb321 --remote_root=/users/adb321/alock --local_root=/Users/amandabaran/Desktop/sss/async_locks/alock --cmd="cd alock/alock && ~/go/bin/bazelisk build -c opt --lock_type=$1 //alock/benchmark/one_lock:main --action_env=BAZEL_CXXOPTS='-std=c++20'"
-  # if [ $? -ne 0 ]; then 
-  #   echo "Build Error: ${result}" 
-  #   exit 1
-  # fi
   echo "Build Complete\n"
   cd ${tmp}
 }
@@ -41,31 +37,23 @@ clean
 echo "Pushing local repo to remote nodes..."
 sync
 
-# Experiments needed to recreate Figure 4 - Budget plot
-
 save_dir="budget_exp"
 
-lock="alock"
+lock="spin"
 log_level='info'
 echo "Building ${lock}..."
 build ${lock}
 
-num_nodes=20
+# Experiments needed to create Figure 1 - RDMA loopback scalability plot
 
-for p_local in .95 .9 .85
-do
-  for num_threads in 4 6 8 10 12
-  do 
-    for max in 100
-    do
-      for remote_budget in 5 10 20 30
-      do
-        for local_budget in 5 10 20 30
-        do
-          num_clients=$(( num_threads*num_nodes ))
-          bazel run //alock/benchmark/one_lock:launch -- -n ${nodefile} -C ${num_clients} --nodes=${num_nodes} --ssh_user=adb321 --lock_type=${lock} --think_ns=0 --runtime=10 --remote_save_dir=${save_dir} --log_level=${log_level} --p_local=${p_local} --threads=${num_threads} --max_key=${max} --local_budget=${local_budget} --remote_budget=${remote_budget} --dry_run=False
-        done
-      done
-    done
+num_nodes=1
+
+for num_threads in 4 8 12 16 24 32 64 96 108 120 132 148 156 164 172
+do 
+  for max in 1000
+  do
+    num_clients=$(( num_threads*num_nodes ))
+    bazel run //alock/benchmark/one_lock:launch -- -n ${nodefile} -C ${num_clients} --nodes=${num_nodes} --ssh_user=adb321 --lock_type=${lock} --think_ns=0 --runtime=10 --remote_save_dir=${save_dir} --log_level=${log_level} --threads=${num_threads} --max_key=${max} --dry_run=False
   done
 done
+
