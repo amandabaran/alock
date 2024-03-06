490d807775053b1950f633a145ecf32e5c0126aa
Amanda Baran <amandabaran9@gmail.com>
2024-03-06 10:47:17 -0500

    updates

--- build/remus/metrics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amanda/sss/remus/metrics
-# Build directory: /home/amanda/sss/alock/build/remus/metrics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
